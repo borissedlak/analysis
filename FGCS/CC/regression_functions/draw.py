db4ed1bb2d4e4c86abe35894b74b6b0ad241a81a
borissedlak <bsedlak01@gmail.com>
2023-10-10 09:43:04 +0200

    Add Orin GPU

--- FGCS/CC/regression_functions/draw.py
+++ FGCS/CC/regression_functions/draw.py
@@ -7,12 +7,13 @@ poly_features = PolynomialFeatures(degree=1)
 
 model_Laptop = joblib.load("models/Laptop.sav")
 model_Xavier = joblib.load("models/Xavier.sav")
+model_Orin = joblib.load("models/Orin.sav")
 
 x_range = np.linspace(1, 16)
 fig, ax = plt.subplots()
 
 for m, gpu, s, c in [(model_Laptop, 0, "Laptop CPU", "red"), (model_Xavier, 0, "Xavier CPU", "green"),
-                     (model_Xavier, 1, "Xavier GPU", "blue")]:
+                     (model_Xavier, 1, "Xavier GPU", "blue"), (model_Orin, 1, "Orin GPU", "black")]:
     input_data = np.column_stack((x_range, np.full(x_range.shape, gpu)))
     y_pred_full = m.predict(poly_features.fit_transform(input_data))
     y_pred_factor = y_pred_full[:, 0] * y_pred_full[:, 1]
